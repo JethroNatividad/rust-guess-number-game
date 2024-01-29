72a4533c6fb672c496dc01cfdbac9a134f023632
JethroNatividad <natividad.jet@gmail.com>
2024-01-29 09:02:14 +0800

    write problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Guess number game, 3 levels. level 1: 1 - 10, Level 2: 1 - 100, Level 3: 1 - 1000.
+// Guess number game, 3 levels. level 1: 1 - 10, Level 2: 1 - 100, Level 3: 1 - 1000. show Too high, Too low, track the attempts.
 
 fn main() {
     println!("Hello, world!");
