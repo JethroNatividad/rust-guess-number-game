641b91d3dba6fafc318629be63032cb42f1756c9
JethroNatividad <natividad.jet@gmail.com>
2024-01-29 09:04:30 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // Guess number game, 3 levels. level 1: 1 - 10, Level 2: 1 - 100, Level 3: 1 - 1000. show Too high, Too low, track the attempts.
+// Inputs: level, guess
+// Process: get level, generate random number, loop, get guess, if > random show Too high, if < random show Too Low. ask if play again.
+// Outputs: Too High, Too Low, You got it in n guesses!
 
 fn main() {
     println!("Hello, world!");
